8e00d8de5e2f9b5a8ed197841f7dce30bc993715
Hannes Leutloff <hannes.leutloff@thenativeweb.io>
2022-03-24 09:11:27 +0100

    fix: Update dependencies.

--- test/unit/typeOfTests.ts
+++ test/unit/typeOfTests.ts
@@ -1,12 +1,12 @@
 import { assert } from 'assertthat';
-import { typeOf } from '../../lib';
 import { values } from '../shared/values';
+import { Type, typeOf } from '../../lib';
 
 suite('typeOf', (): void => {
   for (const [ type, valuesOfType ] of Object.entries(values)) {
     test(`returns the correct type name for values of type ${type}.`, async (): Promise<void> => {
       for (const value of valuesOfType) {
-        assert.that(typeOf(value)).is.equalTo(type);
+        assert.that(typeOf(value)).is.equalTo(type as Type);
       }
     });
   }
